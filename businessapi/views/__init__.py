9d4d02f66637b9a38910a5c07094f0a2dc4807ec
DeAndre Hill <deandre.hill2001@gmail.com>
2023-09-16 10:09:36 -0500

    no

--- businessapi/views/__init__.py
+++ businessapi/views/__init__.py
@@ -2,3 +2,4 @@ from .auth import check_user, register_user
 from .user import UserView
 from .business import BusinessView
 from .business_type import BusinessTypeView
+from .fav_business import FavBusinessView
